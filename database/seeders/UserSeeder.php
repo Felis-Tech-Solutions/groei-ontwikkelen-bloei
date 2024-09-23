cdeec0ca49d2072754fb4510072d59b0f2827069
Max Felis <max.felis11@gmail.com>
2024-09-23 19:39:47 +0200

    Changed the collection to be saved in the database

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,18 +10,10 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate([
-            'name' => 'Max',
-        ], [
-            'email' => 'contact@felis-ts.nl',
-            'password' => bcrypt('password'),
-            'super' => true,
-        ]);
-
-        User::updateOrCreate([
             'name' => 'Daan',
         ], [
             'email' => 'info@dkdesigns.nl',
-            'password' => bcrypt('password'),
+            'password' => config('auth.passwords.password'),
             'super' => true,
         ]);
     }
