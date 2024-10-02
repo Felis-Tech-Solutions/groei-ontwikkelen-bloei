719f19c50a9576da15fa8f2505ae661b5a371a19
Daan Kroft <d.kroft@flooris.nl>
2024-10-02 20:55:13 +0200

    Removed Laravel instance

--- resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Over ons')
-
-@section('content')
-
-@endsection
--- resources/views/default.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Default')
-
-@section('content')
-
-@endsection
